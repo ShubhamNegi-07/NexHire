a23a8577720d7c1c1530f0514716c6e28ffa448c
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-04-15 19:15:16 +0530

    Initial commit - MERN project setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,10 @@ import path from"path"
 import { ENV } from "./lib/env.js";
 
 const app = express();
-th",(req, res)=>{
+
+const __dirname = path.resolve()
+
+app.get("/health",(req, res)=>{
     res.status(200).json({ msg: "api is up and running" });
 });
 
@@ -15,9 +18,7 @@ app.get("/books",(req, res)=>{
 //make our app ready for deployment 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+le(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
