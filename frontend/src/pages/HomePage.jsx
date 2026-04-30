41c82b69b3ee62dfde96acc0caf1e90527e66548
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-04-30 22:39:31 +0530

    feat(auth): update HomePage.jsx imports and Clerk/Lucide-react integration

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+import {
+  ArrowRightIcon,
+  CheckIcon,
+  Code2Icon,
+  SparklesIcon,
+  UsersIcon,
+  VideoIcon,
+  ZapIcon,
+} from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
