b06423719f89966b6d05e634e89d397cf3c7880e
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-05-04 21:40:24 +0530

    feat(problems): Completed ProblemsPage.jsx Page

--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -66,3 +66,31 @@ function ProblemsPage() {
         </div>
 
         {/* STATS FOOTER */}
+        <div className="mt-12 card bg-base-100 shadow-lg">
+          <div className="card-body">
+            <div className="stats stats-vertical lg:stats-horizontal">
+              <div className="stat">
+                <div className="stat-title">Total Problems</div>
+                <div className="stat-value text-primary">{problems.length}</div>
+              </div>
+
+              <div className="stat">
+                <div className="stat-title">Easy</div>
+                <div className="stat-value text-success">{easyProblemsCount}</div>
+              </div>
+              <div className="stat">
+                <div className="stat-title">Medium</div>
+                <div className="stat-value text-warning">{mediumProblemsCount}</div>
+              </div>
+              <div className="stat">
+                <div className="stat-title">Hard</div>
+                <div className="stat-value text-error">{hardProblemsCount}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ProblemsPage;
