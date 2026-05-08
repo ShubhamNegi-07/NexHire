847738f98e66f91bcd283fff48bb363e14b002f9
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-05-08 19:46:10 +0530

    refactor: update problem data in problems.js

--- frontend/src/data/problems.js
+++ frontend/src/data/problems.js
@@ -98,3 +98,31 @@ class Solution {
   // Write your solution here
   
 }
+
+// Test cases
+let test1 = ["h","e","l","l","o"];
+reverseString(test1);
+console.log(test1); // Expected: ["o","l","l","e","h"]
+
+let test2 = ["H","a","n","n","a","h"];
+reverseString(test2);
+console.log(test2); // Expected: ["h","a","n","n","a","H"]`,
+      python: `def reverseString(s):
+    # Write your solution here
+    pass
+
+# Test cases
+test1 = ["h","e","l","l","o"]
+reverseString(test1)
+print(test1)  # Expected: ["o","l","l","e","h"]
+
+test2 = ["H","a","n","n","a","h"]
+reverseString(test2)
+print(test2)  # Expected: ["h","a","n","n","a","H"]`,
+      java: `import java.util.*;
+
+class Solution {
+    public static void reverseString(char[] s) {
+        // Write your solution here
+        
+    }
